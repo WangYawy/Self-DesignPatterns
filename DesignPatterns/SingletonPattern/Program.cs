fd338e546b4376209bfe96caf88fcb0281e691c6
WangYawy <wywangyawy@163.com>
2017-09-18 17:58:50 +0800

    add new project

--- DesignPatterns/SingletonPattern/Program.cs
+++ DesignPatterns/SingletonPattern/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DesignPatterns
+namespace SingletonPattern
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("now");
         }
     }
 }
--- DesignPatterns/SingletonPattern/Properties/AssemblyInfo.cs
+++ DesignPatterns/SingletonPattern/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DesignPatterns")]
+[assembly: AssemblyTitle("SingletonPattern")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesignPatterns")]
+[assembly: AssemblyProduct("SingletonPattern")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("e5d8aeaa-b941-4e96-858c-6e57fe7f5d06")]
+[assembly: Guid("a03e36e7-c753-4e20-a9fa-640e73d14da0")]
 
 // 程序集的版本信息由下列四个值组成: 
 //

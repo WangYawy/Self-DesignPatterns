97ba37424481748ddfe12ac40a44767676120888
WangYawy <wywangyawy@163.com>
2017-09-22 13:35:15 +0800

    组合模式

---
+++ DesignPatterns/10-CompositePattern/Graphics.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CompositePattern
+{
+    /// <summary>
+    /// 图形抽象类
+    /// </summary>
+    abstract public class Graphics
+    {
+        public string Name { get; set; }
+        public Graphics(string name)
+        {
+            this.Name = name;
+        }
+
+        abstract public void Draw();
+        //abstract public void Add(Graphics g);
+        //abstract public void Remove(Graphics g);
+    }
+
+    /// <summary>
+    /// 简单图形-线
+    /// </summary>
+    public class Line : Graphics
+    {
+        public Line(string name) : base(name)
+        { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("画{0}", this.Name);
+        }
+
+        // 因为简单图形在添加或移除其他图形，所以简单图形Add或Remove方法没有任何意义
+        // 如果客户端调用了简单图形的Add或Remove方法将会在运行时抛出异常
+        // 我们可以在客户端捕获该类移除并处理
+        //public override void Add(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Line添加其他图形");
+        //}
+        //public override void Remove(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Line移除其他图形");
+        //}
+    }
+
+    /// <summary>
+    /// 简单图形-圆
+    /// </summary>
+    public class Circle : Graphics
+    {
+        public Circle(string name) : base(name)
+        {
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("画{0}", this.Name);
+        }
+        // 因为简单图形在添加或移除其他图形，所以简单图形Add或Remove方法没有任何意义
+        // 如果客户端调用了简单图形的Add或Remove方法将会在运行时抛出异常
+        // 我们可以在客户端捕获该类移除并处理
+        //public override void Add(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Circle添加其他图形");
+        //}
+        //public override void Remove(Graphics g)
+        //{
+        //    throw new Exception("不能向简单图形Circle移除其他图形");
+        //}
+    }
+
+    public class ComplexGraphics : Graphics
+    {
+        List<Graphics> list = new List<Graphics>();
+        public ComplexGraphics(string name) : base(name)
+        {
+        }
+
+        public void Add(Graphics g)
+        {
+            list.Add(g);
+        }
+
+        public override void Draw()
+        {
+            foreach (var item in list)
+            {
+                item.Draw();
+            }
+        }
+
+        public void Remove(Graphics g)
+        {
+            list.Remove(g);
+        }
+    }
+
+}
---
+++ DesignPatterns/10-CompositePattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CompositePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //组合模式允许你将对象组合成树形结构来表现”部分 - 整体“的层次结构，使得客户以一致的方式处理单个对象以及对象的组合。
+            //下面我们用绘制的例子来详细介绍组合模式，图形可以由一些基本图形元素组成（如直线，圆等），
+            //也可以由一些复杂图形组成（由基本图形元素组合而成），为了使客户对基本图形和复杂图形的调用保持一致，
+            //我们使用组合模式来达到整个目的。
+            //组合模式实现的最关键的地方是——简单对象和复合对象必须实现相同的接口。这就是组合模式能够将组合对象和简单对象进行一致处理的原因。
+
+            Graphics g_line = new Line("线1");
+            Graphics g_circle = new Circle("圆1");
+            g_line.Draw();
+            g_circle.Draw();
+
+            
+            // 复杂图形
+            ComplexGraphics g = new ComplexGraphics("复杂图形");
+            g.Add(g_line);
+            g.Add(g_circle);
+            g.Add(g_line);
+            g.Add(new Circle("圆2"));
+            Console.WriteLine("开始复杂tuxing...");
+            g.Draw();
+        }
+    }
+}

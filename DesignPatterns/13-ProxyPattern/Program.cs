cd2560676ca4e3c036a975ed790da0cb2019ae7e
WangYawy <wywangyawy@163.com>
2017-09-22 17:24:33 +0800

    代理模式

---
+++ DesignPatterns/13-ProxyPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //在现实生活中，如果有同事出国或者朋友出国的情况下，我们经常会拖这位朋友帮忙带一些电子产品或化妆品等东西，
+            //这个场景中，出国的朋友就是一个代理，他（她）是他（她）朋友的一个代理，由于他朋友不能去国外买东西，他却可以，
+            //所以朋友们都托他帮忙带一些东西的。
+
+            // 代理
+            Person proxy = new Friend();
+            proxy.BuyProduct();
+
+        }
+    }
+}
---
+++ DesignPatterns/13-ProxyPattern/Proxy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ProxyPattern
+{
+
+    /// <summary>
+    /// 抽象主题角色
+    /// </summary>
+    abstract public class Person
+    {
+        abstract public void BuyProduct();
+    }
+
+    /// <summary>
+    /// 真是主题角色
+    /// </summary>
+    public class RealBuyPerson : Person
+    {
+        public override void BuyProduct()
+        {
+            Console.WriteLine("买个手机买个电脑");
+        }
+    }
+    /// <summary>
+    /// 代理角色
+    /// </summary>
+    public class Friend : Person
+    {
+        /// <summary>
+        /// 真是主题实例
+        /// </summary>
+        RealBuyPerson real;
+        public override void BuyProduct()
+        {
+            Console.WriteLine("通过代理访问真是实体对象方法");
+            if (real == null)
+            {
+                real = new RealBuyPerson();
+            }
+            this.PreBuy();
+            real.BuyProduct();
+            this.PostBuy();
+        }
+
+
+        // 代理角色执行的操作
+        public void PreBuy()
+        {
+            // 东西较多 列个清单
+            Console.WriteLine("张三手机，李四电脑");
+        }
+
+        public void PostBuy()
+        {
+            Console.WriteLine("买完之后分类，手机是张三的，电脑是李四的");
+        }
+    }
+}

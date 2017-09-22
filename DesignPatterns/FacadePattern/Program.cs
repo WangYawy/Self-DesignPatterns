ff4b9161054faa76caff9ff2d1c15853b879ef8e
WangYawy <wywangyawy@163.com>
2017-09-22 14:18:16 +0800

    外观模式

---
+++ DesignPatterns/FacadePattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 选课，验证是否有课，通知学生
+            var course = "外太空";
+            var student = "Ming";
+            RegistrationFacade facade = new RegistrationFacade();
+            if (facade.RegisterCourse(course, student))
+            {
+                Console.WriteLine("选课成功");
+            }
+            else
+            {
+                Console.WriteLine("选课失败");
+            }
+        }
+    }
+}
---
+++ DesignPatterns/FacadePattern/RegistrationFacade.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    /// <summary>
+    /// 外观类
+    /// </summary>
+    public class RegistrationFacade
+    {
+        private RegisterCourse course;
+        private NofifyStudent nofify;
+
+        public RegistrationFacade()
+        {
+            course = new RegisterCourse();
+            nofify = new NofifyStudent();
+        }
+
+        public bool RegisterCourse(string courseName, string studentName)
+        {
+            if (course.CheckAvailable(courseName))
+            {
+                return nofify.Notify(studentName);
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 相当于子系统A
+    /// </summary>
+    public class RegisterCourse
+    {
+        public bool CheckAvailable(string courseName)
+        {
+            Console.WriteLine("正在验证课程 {0} 是否已满", courseName);
+            return true;
+        }
+    }
+    /// <summary>
+    /// 相当于子系统B
+    /// </summary>
+    public class NofifyStudent
+    {
+        public bool Notify(string studentName)
+        {
+            Console.WriteLine("正在向 {0} 发出通知",studentName);
+            return true;
+        }
+    }
+}

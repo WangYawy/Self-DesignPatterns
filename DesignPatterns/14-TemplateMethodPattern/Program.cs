b1b708f871a6c26aabc284eb64056dedb91a7877
WangYawy <wywangyawy@163.com>
2017-09-22 17:46:50 +0800

    模版方法模式

---
+++ DesignPatterns/14-TemplateMethodPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_TemplateMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //以生活中炒蔬菜为例来实现下模板方法模式。在现实生活中，做蔬菜的步骤都大致相同，
+            //如果我们针对每种蔬菜类定义一个烧的方法，这样在每个类中都有很多相同的代码，为了解决这个问题，
+            //我们一般的思路肯定是把相同的部分抽象出来到抽象类中去定义，具体子类来实现具体的不同部分，
+            //这个思路也正式模板方法的实现精髓所在
+
+
+            Vagetabel spinach = new Spinach();
+            spinach.CookVegetabel();
+            Console.WriteLine();
+            Vagetabel cabbage= new ChineseCabbage();
+            cabbage.CookVegetabel();
+        }
+    }
+}
---
+++ DesignPatterns/14-TemplateMethodPattern/Vagetabel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_TemplateMethodPattern
+{
+    /// <summary>
+    /// 抽象类
+    /// </summary>
+    abstract public class Vagetabel
+    {
+        // 模板方法，不要把模版方法定义为Virtual或abstract方法，避免被子类重写，防止更改流程的执行顺序
+        public void CookVegetabel()
+        {
+            Console.WriteLine("炒蔬菜的一般步骤");
+            this.pourOil();
+            this.HeatOil();
+            this.pourVegetabel();
+            this.stir_fry();
+        }
+
+        public void pourOil()
+        {
+            Console.WriteLine("倒油");
+        }
+
+        public void HeatOil()
+        {
+            Console.WriteLine("把油烧热");
+        }
+        /// <summary>
+        /// 油热之后放菜进去，具体放什么菜，由具体实现决定
+        /// </summary>
+        public abstract void pourVegetabel();
+
+        public void stir_fry()
+        {
+            Console.WriteLine("翻炒");
+        }
+    }
+
+    public class Spinach : Vagetabel
+    {
+        public override void pourVegetabel()
+        {
+            Console.WriteLine("把菠菜倒进去");
+        }
+    }
+
+    public class ChineseCabbage : Vagetabel
+    {
+        public override void pourVegetabel()
+        {
+            Console.WriteLine("把大白菜倒进去");
+        }
+    }
+}

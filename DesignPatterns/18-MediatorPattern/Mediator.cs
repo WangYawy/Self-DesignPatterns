75eecfc5ec29d941e50b527492fd08a4e3147dc8
WangYawy <wywangyawy@163.com>
2017-09-23 15:35:28 +0800

    中介者模式

---
+++ DesignPatterns/18-MediatorPattern/Mediator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_MediatorPattern
+{
+    /// <summary>
+    /// 抽象打牌人
+    /// </summary>
+    public abstract class AbstractCardPartner
+    {
+        public int MoneyCount { get; set; }
+        public AbstractCardPartner()
+        {
+            MoneyCount = 0;
+        }
+
+        public abstract void ChangeCount(int count, AbstractMediator mediator);
+    }
+
+    // 具体打牌人A
+    public class PartnerA : AbstractCardPartner
+    {
+        public override void ChangeCount(int count, AbstractMediator mediator)
+        {
+            mediator.AWin(count);
+        }
+    }
+    // 具体B
+    public class PartnerB : AbstractCardPartner
+    {
+        public override void ChangeCount(int count, AbstractMediator mediator)
+        {
+            mediator.BWin(count);
+        }
+    }
+
+    /// <summary>
+    /// 抽象中介者类
+    /// </summary>
+    public abstract class AbstractMediator
+    {
+        protected AbstractCardPartner A;
+        protected AbstractCardPartner B;
+        public AbstractMediator(AbstractCardPartner a, AbstractCardPartner b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public abstract void AWin(int count);
+        public abstract void BWin(int count);
+    }
+
+    public class MediatorPater : AbstractMediator
+    {
+        public MediatorPater(AbstractCardPartner a, AbstractCardPartner b) : base(a, b)
+        {
+        }
+
+        public override void AWin(int count)
+        {
+            A.MoneyCount += count;
+            B.MoneyCount -= count;
+        }
+
+        public override void BWin(int count)
+        {
+            A.MoneyCount -= count;
+            B.MoneyCount += count;
+
+        }
+    }
+}
---
+++ DesignPatterns/18-MediatorPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_MediatorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractCardPartner patner1 = new PartnerA();
+            AbstractCardPartner patner2 = new PartnerB();
+            AbstractMediator mediator = new MediatorPater(patner1, patner2);
+            patner1.MoneyCount = 100;
+            patner2.MoneyCount = 100;
+
+            patner1.ChangeCount(5, mediator);
+            Console.WriteLine("A.MoneyCount = {0} , B.MoneyCount = {1}", patner1.MoneyCount, patner2.MoneyCount);
+
+            patner2.ChangeCount(50, mediator);
+            Console.WriteLine("A.MoneyCount = {0} , B.MoneyCount = {1}", patner1.MoneyCount, patner2.MoneyCount);
+        }
+    }
+}

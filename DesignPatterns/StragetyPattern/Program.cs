1c8ea431a4c58cf2d5c540f6aa541dc28f0e36d4
WangYawy <wywangyawy@163.com>
2017-09-26 14:52:21 +0800

    策略者模式

---
+++ DesignPatterns/StragetyPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StragetyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //个人所得税
+            InterestOperation operation = new InterestOperation(new PersonalTaxStragety());
+            Console.WriteLine("个人支付的税{0}",operation.GetTax(5000));
+
+            //企业所得税
+            operation = new InterestOperation(new EnterPriseTaxStragety());
+            Console.WriteLine("企业支付的税{0}",operation.GetTax(5000));
+        }
+    }
+}
---
+++ DesignPatterns/StragetyPattern/Stragety.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StragetyPattern
+{
+    // 所得税计算策略
+    public interface ITaxStragety
+    {
+        double CalculateTax(double income);
+    }
+    /// <summary>
+    /// 个人所得税
+    /// </summary>
+    public class PersonalTaxStragety : ITaxStragety
+    {
+        public double CalculateTax(double income)
+        {
+            return income * 0.12;
+        }
+    }
+    /// <summary>
+    /// 企业所得税
+    /// </summary>
+    public class EnterPriseTaxStragety : ITaxStragety
+    {
+        public double CalculateTax(double income)
+        {
+            return (income - 3500) > 0 ? (income - 3500) * 0.045 : 0.0;
+        }
+    }
+
+    public class InterestOperation
+    {
+        private ITaxStragety m_stragety;
+        public InterestOperation(ITaxStragety stragety)
+        {
+            this.m_stragety = stragety;
+        }
+
+        public double GetTax(double income)
+        {
+            return m_stragety.CalculateTax(income);
+        }
+    }
+}
